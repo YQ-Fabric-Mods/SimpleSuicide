e34342c3cdb03aa12684bb10e066c0c6adf4ac42
YQ <yqs112358@gmail.com>
2025-06-19 15:03:37 +0800

    Update to 1.21.6

--- src/main/java/com/yqs112358/SuicideCommand.java
+++ src/main/java/com/yqs112358/SuicideCommand.java
@@ -17,7 +17,7 @@ public final class SuicideCommand {
 
     public static int playerSuicide(ServerCommandSource source) throws CommandSyntaxException {
         ServerPlayerEntity player = source.getPlayerOrThrow();
-        player.kill(player.getServerWorld());
+        player.kill(player.getWorld());
         return Command.SINGLE_SUCCESS;
     }
 }
