94166661c99e1f3c569964eca8e751bb2ed5b00c
YQ <37969157+yqs112358@users.noreply.github.com>
2026-06-28 11:38:09 +0800

    Update to 26.2

--- src/main/java/com/yqs112358/SuicideCommand.java
+++ src/main/java/com/yqs112358/SuicideCommand.java
@@ -3,21 +3,21 @@ package com.yqs112358;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.server.level.ServerPlayer;
 
-import static net.minecraft.server.command.CommandManager.*;
+import static net.minecraft.commands.Commands.literal;
 
 
 public final class SuicideCommand {
-    public static void register(CommandDispatcher<ServerCommandSource> dispatcher){
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher){
         dispatcher.register(literal("suicide")
                 .executes(ctx -> playerSuicide(ctx.getSource())));
     }
 
-    public static int playerSuicide(ServerCommandSource source) throws CommandSyntaxException {
-        ServerPlayerEntity player = source.getPlayerOrThrow();
-        player.kill(player.getEntityWorld());
+    public static int playerSuicide(CommandSourceStack source) throws CommandSyntaxException {
+        ServerPlayer player = source.getPlayerOrException();
+        player.kill(player.level());
         return Command.SINGLE_SUCCESS;
     }
 }
